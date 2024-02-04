b4f7ffa0980ace86a9adeec09a6156d41448650f
mihey <148856334+My-Alter@users.noreply.github.com>
2024-02-04 14:42:58 +0500

    Add files via upload

--- Playing Game/script.js
+++ Playing Game/script.js
@@ -193,6 +193,11 @@ function attack() {
 	health -= monsters[fighting].level;
 	monsterHealth -=
 		weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+	healthText.innerText = health;
+	monsterHealthText.innerText = monsterHealth;
+	if (health <= 0) {
+		lose();
+	}
 }
 
 function dodge() {}
