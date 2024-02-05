a6f55d549f44075cbd1c224c254c06ccaffa541b
mihey <148856334+My-Alter@users.noreply.github.com>
2024-02-05 18:35:56 +0500

    Add files via upload

---
+++ CalorieCounter/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const calorieCounter = document.getElementById("calorie-counter");
+const budgetNumberInput = document.getElementById("budger");
+const entryDropdown = document.getElementById("entry-dropdown");
+const addEntryButton = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
+let isError = false;
+
+function cleanInputString(str) {
+	const strArray = str.split("");
+	let cleanStrArray = [];
+	for (let i = 0; index < array.length; i++) {
+		if (!["+", "-", " "].includes(strArray[i])) {
+			cleanStrArray.push(strArray[i]);
+		}
+	}
+}
