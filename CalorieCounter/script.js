ab8ca8827d389f103d134ad9f13863ca461cb643
mihey <148856334+My-Alter@users.noreply.github.com>
2024-06-16 10:04:57 +0500

    Add files via upload

--- CalorieCounter/script.js
+++ CalorieCounter/script.js
@@ -19,8 +19,7 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-	const targetId = "#" + entryDropdown.value;
 	const targetInputContainer = document.querySelector(
-		targetId + " .input-container"
+		`#${entryDropdown.value} .input-container`
 	);
 }
